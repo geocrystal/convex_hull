2091fcfb4f1d33366e435fde990ce9f125ca7f3f
Anton Maminov <anton.linux@gmail.com>
2020-06-15 14:15:06 +0300

    v0.2.0

--- src/convex_hull.cr
+++ src/convex_hull.cr
@@ -3,7 +3,7 @@ require "./convex_hull/jarvis_march"
 require "./convex_hull/graham_scan"
 
 module ConvexHull
-  VERSION = "0.1.0"
+  VERSION = "0.2.0"
 
   alias Number = Int32 | Float32 | Float64
 end
